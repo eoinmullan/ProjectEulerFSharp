7bd6ccb6ac38b31028fc5d61dba110baebe17e89
eoinmullan <eoinmullan@hotmail.com>
2015-08-19 23:47:14 +0100

    Adding project and solution files and the solution to the first problem.

---
+++ ProjectEulerFSharp/Problem001.fs
@@ -0,0 +1,12 @@
+﻿module Problem001
+
+let isMultipleOf3Or5 x =
+    match x with
+    | x when x % 3 = 0 -> true
+    | x when x % 5 = 0 -> true
+    | _                -> false
+
+let answer =
+    [1..999]
+    |> List.filter isMultipleOf3Or5
+    |> List.sum
---
+++ ProjectEulerFSharp/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+module ProjectEuler
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Problem 1, answer: %d" Problem001.answer
+    0 // return an integer exit code
